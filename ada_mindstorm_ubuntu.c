77acce2d474af5bcc01911b8e4d8e27fb19be300
difo23 <diffozanzan@gmail.com>
2020-09-11 15:58:02 -0400

    first commit

---
+++ ada_mindstorm_ubuntu.c
@@ -0,0 +1,2 @@
+http://www.dit.upm.es/~str/proyectos/mindstorms/2012/adaformindstorms-linux-2012-x86-gdb-bin.tar.gz
+/usr/local/gnatmindstorms2012/lib/gcc/arm-eabi/4.5.4/rts-ravenscar-sfp/ravenscar/
---
+++ hola.adb
@@ -0,0 +1,11 @@
+with Ada.Text_IO;
+use Ada.Text_IO;
+
+
+procedure hola is
+begin
+   loop
+      put("Hola");
+      new_line;
+   end loop;
+end hola;
